dd862bcee4a2cd7f654cdcbfd8e4f94e5078611c
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-09-22 00:26:31 -0400

    create styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     var shows = ["The Office", "Bobs Burgers", "Friends", "New Girl", "It's Always Sunny in Philadelphia"];
 
     function startShow () {
+        $('#shows-view').empty();
         shows = $(this).attr("src");
-        // var queryURL = "https:api.giphy.com/v1/gifs/search?api_key=LcJytGEKV6AKmIud1ZiLwqA1hiqyAxiz&q=" + shows + "&limit=10&rating=PG";
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + shows + "&api_key=LcJytGEKV6AKmIud1ZiLwqA1hiqyAxiz&limit=10&rating=PG";
   
         $.ajax({
@@ -16,11 +16,11 @@ $(document).ready(function(){
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-              var showDiv = $("<div class='col-6'>");
+              var showDiv = $("<div class='col-sm-8 col-lg-5 m-3'>");
               var rate = $("<p>").text("Rating: " + results[i].rating);
               // console.log(gifrate);
 
-              var showImage = $("<img>");
+              var showImage = $("<img class='module-border-wrap'>");
 
               var stills = results[i].images.fixed_height_still.url;
               var animates = results[i].images.fixed_height.url;
@@ -30,14 +30,14 @@ $(document).ready(function(){
               showImage.attr("data-animate", animates);
               showImage.attr("src", stills);
               $("#shows-view").prepend(showDiv);
-              showDiv.append(rate);
               showDiv.append(showImage);
+              showDiv.append(rate);
 
             }
-            $(document).on("click", 'img', function() {
-              var status = $(this).attr('data-state');
+            $('img').on("click", function() {
+              var state = $(this).attr('data-state');
 
-              if (status === "still") {
+              if (state === "still") {
                 var toggle = $(this).attr("data-animate");
                 $(this).attr('src', toggle);
                 $(this).attr("data-state", "animated")
@@ -52,11 +52,10 @@ $(document).ready(function(){
           });
 
     };
-
     function givenButtons() {
-      $("#buttons-view").empty();
+      // $("#buttons-view").empty();
       for (var i = 0; i < shows.length; i++) {
-        var btns = $("<button class='btn btn-outline-secondary m-1'>");
+        var btns = $("<button class='btn m-1'>");
         btns.addClass("show-btn");
         btns.attr("src", shows[i]);
         btns.text(shows[i]);
@@ -64,13 +63,16 @@ $(document).ready(function(){
       }
     }
 
-
     $("#add-show").on("click", function(event) {
-      event.preventDefault();
-      var add = $("#show-input").val().trim();
-      shows.push(add);
-      givenButtons();
-      $("#show-input").val('');
+      if ($("#show-input").val() === ""){
+        return;
+      } else { event.preventDefault();
+        var add = $("#show-input").val().trim();
+        shows = [];
+        shows.push(add);
+        givenButtons();
+        $("#show-input").val('');}
+    
     });
 
     givenButtons();
